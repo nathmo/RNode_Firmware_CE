b6e5384de9229cd05d903a725c7d7938c671a3b4
Nathann Morand <natmo@hotmail.ch>
2024-08-04 23:55:09 +0200

    Update Radio.h

--- Radio.h
+++ Radio.h
@@ -454,7 +454,7 @@ private:
 
 class sx127x : public RadioInterface {
 public:
-  sx127x(uint8_t index, SPIClass spi, int ss, int sclk, int mosi, int miso, int reset, int dio0, int busy);
+  sx127x(uint8_t index, SPIClass& spi, int ss, int sclk, int mosi, int miso, int reset, int dio0, int busy);
 
   int begin();
   void end();
@@ -529,7 +529,7 @@ private:
 
 private:
   SPISettings _spiSettings;
-  SPIClass _spiModem;
+  SPIClass&  _spiModem;
   int _ss;
   int _sclk;
   int _mosi;
